2a6a877b86dd70fe86747edccd85d24e223f5c27
interfaxz <128165609+interfaxz@users.noreply.github.com>
2025-04-27 21:41:26 +0300

    Update game.js

--- game.js
+++ game.js
@@ -14,69 +14,48 @@ let player;
 let cursors;
 
 function preload() {
-    this.load.image("bitcoin", "assets/bitcoin.png");
+    // Загружаем спрайт-лист для анимации Pacman
+    // Предполагается, что у вас есть файл с несколькими кадрами
+    this.load.spritesheet("bitcoin", "assets/bitcoin_sheet.png", {
+        frameWidth: 32,  // Ширина одного кадра
+        frameHeight: 32, // Высота одного кадра
+        endFrame: 3      // Количество кадров
+    });
+    
     this.load.image("sec", "assets/sec.png");
 }
-this.anims.create({
-    key: "chomp",
-    frames: this.anims.generateFrameNumbers("bitcoin", { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1
-});
-player.play("chomp");
-
-this.anims.create({
-    key: "chomp",
-    frames: this.anims.generateFrameNumbers("bitcoin", { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1
-});
-player.play("chomp");
 
 function create() {
-    // Добавляем спрайты
+    // Создаем спрайт игрока
     player = this.add.sprite(400, 300, "bitcoin");
+    
+    // Создаем анимацию
+    this.anims.create({
+        key: "chomp",
+        frames: this.anims.generateFrameNumbers("bitcoin", { 
+            start: 0, 
+            end: 3 
+        }),
+        frameRate: 10,
+        repeat: -1
+    });
+    
+    // Запускаем анимацию
+    player.play("chomp");
+
+    // Создаем призрака
     this.sec = this.add.sprite(200, 200, "sec");
 
     // Включаем управление клавиатурой
     cursors = this.input.keyboard.createCursorKeys();
 }
 
-function create() {
-    // Создаем спрайт игрока (уже есть у тебя)
-    player = this.add.sprite(400, 300, "bitcoin");
-
-    // --- НОВЫЙ КОД: Настройка анимации ---
-    this.anims.create({
-        key: "chomp", // Название анимации
-        frames: [
-            { key: "bitcoin", frame: 0 }, // Кадр 1: рот закрыт
-            { key: "bitcoin", frame: 1 }, // Кадр 2: рот полуоткрыт
-            { key: "bitcoin", frame: 2 }  // Кадр 3: рот открыт
-        ],
-        frameRate: 10, // Скорость (кадров в секунду)
-        repeat: -1     // Бесконечный повтор
-    });
-
-    // Запускаем анимацию
-    player.play("chomp");
-}
-
 function update() {
     // Движение игрока
     if (cursors.left.isDown) {
         player.x -= 3;
+        player.flipX = false; // Ориентация спрайта
     } else if (cursors.right.isDown) {
         player.x += 3;
+        player.flipX = true;
     }
-    if (cursors.up.isDown) {
-        player.y -= 3;
-    } else if (cursors.down.isDown) {
-        player.y += 3;
-    }
-
-    // Движение SEC (простой AI)
-    this.sec.x += 0.5;
-}
-
-const game = new Phaser.Game(config);
